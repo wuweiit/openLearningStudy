ffa5e2e84dc11db0b76c892c9aac4a207a2137fd
wuw <903595558@qq.com>
2017-03-05 18:04:12 +0800

    Initial commit

---
+++ StudyChromeApp/script.js
@@ -0,0 +1,28 @@
+﻿
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    alert(tab.url);
+})
+ 
+
+function autoStart(){
+	
+	alert(" dsad ");
+}
+$(function(){
+	
+	
+	
+	
+$("#taskItem").click(function(){
+	
+	alert();
+});
+	
+	
+	
+})
+console.log($("#tdRemark").text());
+
+if($("#tdRemark").text().indexOf("已经学习完毕") != -1){
+	alert("学习完成了的哈@");
+}
---
+++ StudyChromeApp/study.js
@@ -0,0 +1,12 @@
+function(){
+
+	if($("#tdRemark").text().indexOf("已经学习完毕") != -1){
+		alert("学习完成了的哈@");
+	}
+		
+		
+	
+	
+
+
+}();

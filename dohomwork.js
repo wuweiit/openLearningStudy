24038f32d3a251d3e1663961fbf6fd75256b0d01
wuw <Leleyun1029>
2017-12-14 13:31:06 +0800

    支持多选答案的自动答题

--- dohomwork.js
+++ dohomwork.js
@@ -26,31 +26,64 @@ function doHomework(answerStr){
     var questionNode = $("#tblDataList>tbody");
     var rows = questionNode.rows;
 
-    for(var i=0; i<rows.length; i++) {
-        var item = rows[i];
-        var right = ans[i];//正确答案 A =65
-        if(!right){
-            continue;// 不存在就继续走
+
+        for(var i=0; i<rows.length; i++) {
+            var item = rows[i];
+            var right = ans[i];//正确答案 A =65
+            if(!right){
+                continue;// 不存在就继续走
+            }
+            console.log(right);
+            if(right.length <= 1) {// 单项选择
+
+                console.log(i+".单选题:"+right);
+
+                var rightIndex = right.charCodeAt() - 65;
+                // 答案里列表
+                var answerListNode = item.children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[1].children[0].children[0].children[0].children[0]
+
+                // 选中答案
+                try{// 尝试选择判断题
+                    answerListNode.rows[rightIndex].children[0].children[0].click();
+                }catch(e){}
+                // 尝试选择单选题
+                try{
+                    answerListNode.rows[rightIndex].children[0].children[0].click();
+                }catch(e){}
+
+
+            }else{// 多选题
+
+                console.log(i+".多选题:"+right);
+
+                for(var j=0;j<right.length; j++){
+                    var crooet = right[j];
+                    var rightIndex = crooet.charCodeAt() - 65;
+                    // 答案里列表
+                    var answerListNode = item.children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[1].children[0].children[0].children[0].children[0]
+
+
+
+
+
+                    // 选中答案
+                    try{// 尝试选择判断题
+                        answerListNode.rows[rightIndex].children[0].children[0].click();
+                    }catch(e){console.log(e)}
+                }
+
+
+            }
+
+
+
+
         }
-        var rightIndex = right.charCodeAt() - 65;
-        // 答案里列表
-        var answerListNode = item.children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[1].children[0].children[0].children[0].children[0]
 
 
-        // 选中答案
-        try{// 尝试选择判断题
-            console.log(answerListNode);
-            answerListNode.rows[rightIndex].children[0].children[0].click();
-        }catch(e){}
-        // 尝试选择单选题
-        try{
-            answerListNode.rows[rightIndex].children[0].children[0].click();
-        }catch(e){}
-
-    }
 }
 
 // 做作业吧
-doHomework("D|B|B|B|C|C|B|C|B|D|A|D|D|C|A|B|B|B|A|D");
+doHomework("AB|ABCD|C|B|D|B|D|ABD|C|ABC|ABD|ABCD|ABCD|A|ACD|B|C|ABCD|ABCD|C");
 
 

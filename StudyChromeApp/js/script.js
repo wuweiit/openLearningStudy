b833870694106ae6393f7c1c1d0ea0f6880aff59
wuwei <wuwei@byitgroup.com>
2019-04-17 14:16:19 +0800

    新增插件支持周期模式和自动模式，可根据课程执行反馈调整合适的模式；

--- StudyChromeApp/js/script.js
+++ StudyChromeApp/js/script.js
@@ -9,10 +9,32 @@
 $(function(){
 
 
+    // 读取模式
+
+    chrome.storage.local.get('mode', function(obj){
+        if(obj.mode){
+            $("#model_" + obj.mode).attr("checked", true);
+            if(obj.mode == 1){
+                $("#inputTimeRoundEl").hide();
+            }
+        }else{
+            chrome.storage.local.set({'mode': 1});
+            $("#inputTimeRoundEl").hide();
+        }
+    });
+    chrome.storage.local.get('time', function(obj){
+        chrome.storage.local.set({'time': obj.time});
+        $("#inputTimeRound").val(obj.time);
+    });
+
+
+
+
+
+
     $("#onlineStudy").click(function(){
         window.open('http://www.uestcedu.com/ifree/console/index.html');
     });
-
 
 
 
@@ -22,8 +44,36 @@ $(function(){
 
 
 
+
+
     $("#site").click(function(){
         window.open('http://www.yl-blog.com/');
+    });
+
+
+
+
+
+
+    $("input[name='model']").click(function(){
+        if(this.value == 1){ // 自动模式
+            chrome.storage.local.set({'mode': 1});
+            $("#inputTimeRoundEl").hide();
+        } else if(this.value ==2){ // 周期模式
+            chrome.storage.local.set({'mode': 2});
+            $("#inputTimeRoundEl").show();
+        }
+
+        chrome.notifications.create('msg_' + Math.random()*10000000, {
+            type: 'basic',
+            title: "消息通知",
+            message: "注意：切换模式需要刷新浏览器！",
+            iconUrl: "images/logo.png"
+        });
+    });
+
+    $("#inputTimeRound").change(function(){
+        chrome.storage.local.set({'time': parseInt(this.value)});
     });
 
 

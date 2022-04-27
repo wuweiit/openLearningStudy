32e6973748be9dc4f1c9e16fbd02d9d8032311c6
huiyixin <13088339090@163.com>
2022-04-27 09:26:00 +0800

    界面完善 表格答案格式完善

--- StudyChromeApp/js/autostudy.js
+++ StudyChromeApp/js/autostudy.js
@@ -1,235 +1,202 @@
-﻿
-var test = parent.document.getElementById("w_code");
-if(test){
-
-    // 判断是不是左边的树
-    if($("#divTree").length > 0){
-        var model = 1;
-        chrome.storage.local.get('mode',function(str){
-            model = str.mode;
-            if(model == 1){
-                console.log("自动学习模式。。。");
-                mode1();
-            } else if(mode = 2) {
-                console.log("周期学习模式。。。");
-                mode2();
-            }
-        });
-    }
+﻿var test = parent.document.getElementById("w_code");
+if (test) {
+   // 判断是不是左边的树
+   if ($("#divTree").length > 0) {
+      var model = 1;
+      chrome.storage.local.get("mode", function (str) {
+         model = str.mode;
+         if (model == 1) {
+            console.log("自动学习模式。。。");
+            mode1();
+         } else if ((mode = 2)) {
+            console.log("周期学习模式。。。");
+            mode2();
+         }
+      });
+   }
 }
-
-
-
-
-
 
 /**
  * 自动学习模式
  */
 function mode1() {
-    var els = $("#btnNext");
-    if (els.length > 0) {
+   var els = $("#btnNext");
+   if (els.length > 0) {
+      console.log("找到了下一个按钮！");
 
-        console.log("找到了下一个按钮！");
+      setTimeout(function () {
+         // var time = getTime();
+         startStudy();
+      }, 1000);
+   } else {
+      setTimeout(function () {
+         var link = $("a", $(".notattempt").first().parents("tr").first());
+         if (link.length < 1) {
+            link = $("a", $(".incomplete").first().parents("tr").first());
+         }
+         if (link.length > 0) {
+            link[0].click();
+         }
+      }, 2000);
+   }
 
+   /**
+    * 获取学习时间
+    */
+   function getTime() {
+      var text = $(window.parent["w_lms_content"].document)
+         .find("#tdRemark")
+         .text();
+      var start = text.indexOf("最少要求学习");
+      var end = text.indexOf("秒");
 
-        setTimeout(function () {
+      var time = parseInt(text.substring(start + 6, end));
 
-            // var time = getTime();
-            startStudy();
+      console.log("读取到视频的学习时间为：" + time);
 
+      return time;
+   }
 
-        }, 1000);
-    }  else {
-        setTimeout(function () {
-            var link = $('a', $('.notattempt').first().parents('tr').first());
-            if (link.length < 1) {
-                link = $('a', $('.incomplete').first().parents('tr').first());
-            }
-            if (link.length > 0) {
-                link[0].click();
-            }
-        }, 2000);
-    }
+   /**
+    * 获取当前的学习进度是否完成
+    * @returns {*|jQuery}
+    */
+   function getStatus() {
+      var html = $(window.parent["w_lms_content"].document).find("#tdRemark")[0]
+         .innerHTML;
+      if (html && html.indexOf("completed") != -1) {
+         return true;
+      }
+      return false;
+   }
 
+   /**
+    * 开始学习
+    * @param time
+    */
+   function startStudy() {
+      /**
+       * 该自动学习脚本仅用于学习研究，未经作者同意不得用于商业用途。
+       *
+       * @author marker
+       * @date 2017-03-05
+       * @type {number}
+       */
 
-    /**
-     * 获取学习时间
-     */
-    function getTime() {
+      /**
+       * 自动学习函数
+       */
+      var myInterval;
+      var i = 0;
 
-        var text = $(window.parent["w_lms_content"].document).find("#tdRemark").text();
-        var start = text.indexOf('最少要求学习');
-        var end = text.indexOf('秒');
+      function study() {
+         i++;
+         console.log("自动正在学习中...");
 
-        var time = parseInt(text.substring(start + 6, end));
+         if (!($("#StudyStatus").length > 0)) {
+            $("body").append(
+               "<div id='StudyStatus' style='height: 30px; line-height: 30px;background: #e1dfe2;  width: 100%;  text-align: center;position: fixed; z-index: 11111;font-weight: bold; bottom: 0px;  color: red'>自动正在学习中...</div>"
+            );
+         }
 
-        console.log("读取到视频的学习时间为：" + time);
+         if (i % 2 == 1) {
+            $("#StudyStatus").html("自动正在学习中...");
+         } else {
+            $("#StudyStatus").html("自动正在学习中&nbsp; &nbsp;");
+         }
 
-        return time;
-    }
+         if (getStatus()) {
+            // 判断是否学习完成
+            console.log("学习完成...");
 
+            if (window.JQUERY_CUSTERM("#btnNext")) {
+               console.log("进入下一章节...");
+               window.JQUERY_CUSTERM("#btnNext").click();
 
-    /**
-     * 获取当前的学习进度是否完成
-     * @returns {*|jQuery}
-     */
-    function getStatus() {
-        var html = $(window.parent["w_lms_content"].document).find("#tdRemark")[0].innerHTML;
-        if (html && html.indexOf("completed") != -1) {
-            return true;
-        }
-        return false;
-    }
+               // 清除定时任务
+               clearInterval(myInterval);
 
-
-    /**
-     * 开始学习
-     * @param time
-     */
-    function startStudy() {
-
-        /**
-         * 该自动学习脚本仅用于学习研究，未经作者同意不得用于商业用途。
-         *
-         * @author marker
-         * @date 2017-03-05
-         * @type {number}
-         */
-
-
-        /**
-         * 自动学习函数
-         */
-        var myInterval;
-        var i = 0;
-
-        function study() {
-            i++;
-            console.log("自动正在学习中...");
-
-            if (!($("#StudyStatus").length > 0)) {
-                $('body').append("<div id='StudyStatus' style='height: 30px; line-height: 30px;background: #e1dfe2;  width: 100%;  text-align: center;position: fixed; z-index: 11111;font-weight: bold; bottom: 0px;  color: red'>自动正在学习中...</div>")
-            }
-
-            if ((i % 2) == 1) {
-                $('#StudyStatus').html('自动正在学习中...');
+               // 延时执行
+               setTimeout(function () {
+                  startRun();
+               }, 1000);
             } else {
-                $('#StudyStatus').html('自动正在学习中&nbsp; &nbsp;');
+               console.error("没有找到【下一个】按钮");
+               console.log("正在关闭周期循环管理器！");
+               clearInterval(myInterval);
+
+               $("#StudyStatus").css("color", "green");
+               $("#StudyStatus").html("学习完成!");
             }
+         }
+      }
 
+      function startRun() {
+         console.log("【下一个】按钮获取焦点");
+         window.JQUERY_CUSTERM("#btnNext", window.frames["w_main"]).focus();
+         myInterval = setInterval(study, 1000);
+      }
 
-            if (getStatus()) { // 判断是否学习完成
-                console.log("学习完成...");
+      window.JQUERY_CUSTERM = $;
+      console.log("自动学习脚本启动...");
 
-                if (window.JQUERY_CUSTERM("#btnNext")) {
-                    console.log("进入下一章节...")
-                    window.JQUERY_CUSTERM("#btnNext").click();
-
-                    // 清除定时任务
-                    clearInterval(myInterval);
-
-                    // 延时执行
-                    setTimeout(function () {
-                        startRun();
-                    }, 1000);
-
-
-                } else {
-                    console.error("没有找到【下一个】按钮");
-                    console.log("正在关闭周期循环管理器！");
-                    clearInterval(myInterval);
-
-                    $("#StudyStatus").css("color", "green");
-                    $("#StudyStatus").html("学习完成!");
-                }
-
-            }
-        };
-
-
-        function startRun() {
-            console.log("【下一个】按钮获取焦点")
-            window.JQUERY_CUSTERM("#btnNext", window.frames['w_main']).focus();
-            myInterval = setInterval(study, 1000);
-        }
-
-
-        window.JQUERY_CUSTERM = $;
-        console.log("自动学习脚本启动...");
-
-
-        setTimeout(startRun, 1000);
-
-
-    }
+      setTimeout(startRun, 1000);
+   }
 }
-
-
-
 
 /**
  * 周期学习模式
  */
-function mode2(){
+function mode2() {
+   /* 全局变量配置 */
+   var STUDY_DELAY_TIME = 1; // 延时启动。单位：秒
+   var STUDY_PERIOD_TIME = 60; // 下一章节周期 单位：秒
 
-    /* 全局变量配置 */
-    var STUDY_DELAY_TIME  = 1; // 延时启动。单位：秒
-    var STUDY_PERIOD_TIME = 60; // 下一章节周期 单位：秒
+   var myInterval;
 
-    var myInterval;
+   /**
+    * 监听周期值变化
+    */
+   chrome.storage.onChanged.addListener(function callback(changes, name) {
+      if ("local" == name) {
+         if (changes.time) {
+            STUDY_PERIOD_TIME = changes.time.newValue;
 
-    /**
-     * 监听周期值变化
-     */
-    chrome.storage.onChanged.addListener(function callback(changes, name){
-        if('local' == name){
-            if(changes.time){
-
-                STUDY_PERIOD_TIME = changes.time.newValue;
-
-                clearInterval(myInterval);
-                myInterval = setInterval(study, STUDY_PERIOD_TIME   * 1000);
-
-            }
-        }
-    });
-
-
-
-    chrome.storage.local.get('time',function(str){
-        STUDY_PERIOD_TIME = str.time;
-        runStudy();
-    });
-
-
-    /**
-     * 自动学习函数
-     */
-    function study(){
-        // 10秒后开始
-        if(window.JQUERY_CUSTERM ("#btnNext")){
-            console.log("进入下一章节...")
-            window.JQUERY_CUSTERM("#btnNext").click();
-        } else {
-            console.log(window.JQUERY_CUSTERM ("#btnNext"));
-            console.error("没有找到【下一个】按钮");
-            console.log("正在关闭周期循环管理器！");
             clearInterval(myInterval);
-        }
-    };
-    function startRun(){
-        console.log("【下一个】按钮获取焦点")
-        window.JQUERY_CUSTERM("#btnNext", window.frames['w_main']).focus();
-        myInterval = setInterval(study, STUDY_PERIOD_TIME   * 1000);
-    }
+            myInterval = setInterval(study, STUDY_PERIOD_TIME * 1000);
+         }
+      }
+   });
 
+   chrome.storage.local.get("time", function (str) {
+      STUDY_PERIOD_TIME = str.time;
+      runStudy();
+   });
 
-    function runStudy(){
-        window.JQUERY_CUSTERM = $;
-        console.log("自动学习脚本启动...")
-        setTimeout(startRun, STUDY_DELAY_TIME * 1000);
-    }
+   /**
+    * 自动学习函数
+    */
+   function study() {
+      // 10秒后开始
+      if (window.JQUERY_CUSTERM("#btnNext")) {
+         console.log("进入下一章节...");
+         window.JQUERY_CUSTERM("#btnNext").click();
+      } else {
+         console.log(window.JQUERY_CUSTERM("#btnNext"));
+         console.error("没有找到【下一个】按钮");
+         console.log("正在关闭周期循环管理器！");
+         clearInterval(myInterval);
+      }
+   }
+   function startRun() {
+      console.log("【下一个】按钮获取焦点");
+      window.JQUERY_CUSTERM("#btnNext", window.frames["w_main"]).focus();
+      myInterval = setInterval(study, STUDY_PERIOD_TIME * 1000);
+   }
 
+   function runStudy() {
+      window.JQUERY_CUSTERM = $;
+      console.log("自动学习脚本启动...");
+      setTimeout(startRun, STUDY_DELAY_TIME * 1000);
+   }
 }
-
--- StudyChromeApp/js/homework.js
+++ StudyChromeApp/js/homework.js
@@ -1,159 +1,187 @@
-﻿
-$(function(){
-    // 作业答案
-    if($("#_block_exam_1").length > 0){
-        homeworkAnswer();
-    }
+﻿$(function () {
+   // 作业答案
+   if ($("#_block_exam_1").length > 0) {
+      homeworkAnswer();
+   } else if ($("#_block_content_exam_2").length > 0) {
+      fillingInput($("#_block_content_exam_2"));
+   }
 
-    // 写入答案
-    if($("#tblDataList").length > 0){
+   // 写入答案
+   let tblDataList = $("#tblDataList").length > 0;
+   let _block_content_exam = $("#_block_content_exam").length > 0;
 
-        /**
-         * 监听周期值变化
-         */
-        chrome.storage.onChanged.addListener(function callback(changes, name){
-            if('local' == name){
-                if(changes.writeAnswert){
-                    if($("#_block_exam_1").text().indexOf("查看考卷") == -1) { // 不是查看考卷才写
-                        chrome.storage.local.get('answers',function(str){
-                            answers = str.answers;
-                            if(answers){
-                                // 做作业吧
-                                doHomework(answers);
-                                alert("答题完成！")
-                            }
-                        });
-                    }
-                }
+   if (tblDataList || _block_content_exam) {
+      /**
+       * 监听周期值变化
+       */
+      chrome.storage.onChanged.addListener(function callback(changes, name) {
+         if ("local" == name) {
+            if (changes.writeAnswert) {
+               if ($("#_block_exam_1").text().indexOf("查看考卷") == -1) {
+                  chrome.storage.local.get("answers", function (str) {
+                     if (!str.answers) return;
+                     answers = str.answers;
+                     console.log(answers);
+                     // 做作业吧
+                     if (tblDataList) {
+                        doHomework(answers);
+                     } else if (_block_content_exam) {
+                        doFilling(answers);
+                     }
+                     alert("答题完成！");
+                  });
+               }
             }
-        });
-    }
-
+         }
+      });
+   }
 });
 
+//表单选择题 写入
+function homeworkAnswer() {
+   var listTable = $("#_block_content_exam_1>form>table>tbody")[0];
+   var rows = listTable.rows;
+   var results = [];
+   for (var i = 0; i < rows.length; i++) {
+      console.log("current index " + i);
+      var item = rows[i];
 
+      var tagName =
+         item.children[1].children[1].children[0].children[1].children[0]
+            .children[0].tagName;
+      var text = "";
+      console.log("current tagName= " + tagName);
+      if ("SPAN" == tagName) {
+         try {
+            text =
+               item.children[1].children[1].children[0].children[1].children[0]
+                  .children[0].children[0].children[1].innerText;
+         } catch (e) {}
+         try {
+            item.children[1].children[1].children[0].children[1].children[0]
+               .children[0].children[1].innerText;
+         } catch (e) {}
+      } else {
+         text =
+            item.children[1].children[1].children[0].children[1].children[0]
+               .children[1].innerText;
+      }
 
-function homeworkAnswer(){
-    var listTable = $("#_block_content_exam_1>form>table>tbody")[0]
-    var rows = listTable.rows;
-    var results = [];
-    for(var i=0; i<rows.length; i++){
-        console.log("current index "+ i)
-        var item = rows[i];
+      // "[参考答案：B]  分值：5"
 
-        var tagName = item.children[1].children[1].children[0].children[1].children[0].children[0].tagName;
-        var text = "";
-        console.log("current tagName= "+ tagName)
-        if("SPAN" == tagName){
-            try{
-                text = item.children[1].children[1].children[0].children[1].children[0].children[0].children[0].children[1].innerText;
-            } catch(e) {}
-            try{
-                item.children[1].children[1].children[0].children[1].children[0].children[0].children[1].innerText;
-            } catch(e) {}
-        }else{
-            text = item.children[1].children[1].children[0].children[1].children[0].children[1].innerText;
-        }
+      var index_a = text.indexOf("：") + 1;
+      var index_b = text.indexOf("]");
+      var result = text.substring(index_a, index_b);
 
+      results.push(result);
+   }
+   console.log(results.join("|"));
 
-
-        // "[参考答案：B]  分值：5"
-
-        var index_a = text.indexOf("：")+1;
-        var index_b = text.indexOf("]");
-        var result = text.substring(index_a, index_b);
-
-        results.push(result);
-    }
-    console.log(results.join("|"));
-
-
-    chrome.storage.local.set({'answers': results.join("|") },function(){
-        alert("答案读取成功！");
-    });
-
+   chrome.storage.local.set({ answers: results.join("|") }, function () {
+      alert("答案读取成功！");
+   });
 }
 
+/*
+ * 填空选择题
+ * @author jiuyin
+ * @param blank
+ */
 
-
-
-
+function fillingInput(el) {
+   let str = el[0].innerText;
+   let fillingAnswer = str.match(/[a-z]/gi);
+   chrome.storage.local.set({ answers: fillingAnswer }, function () {
+      alert("答案读取成功！");
+   });
+}
 
 /**
  * 自动做作业函数
  * @author marker
  * @param answerStr
  */
-function doHomework(answerStr){
-    var ans = answerStr.split("|");// 答案列表
-    var questionNode = $("#tblDataList>tbody")[0];
-    var rows = questionNode.rows;
+function doHomework(answerStr) {
+   var ans = answerStr.split("|"); // 答案列表
+   var questionNode = $("#tblDataList>tbody")[0];
+   var rows = questionNode.rows;
 
+   for (var i = 0; i < rows.length; i++) {
+      var item = rows[i];
+      var right = ans[i]; //正确答案 A =65
+      if (!right) {
+         continue; // 不存在就继续走
+      }
+      console.log(right);
+      if (right.length <= 1) {
+         // 单项选择
 
-    for(var i=0; i<rows.length; i++) {
-        var item = rows[i];
-        var right = ans[i];//正确答案 A =65
-        if(!right){
-            continue;// 不存在就继续走
-        }
-        console.log(right);
-        if(right.length <= 1) {// 单项选择
+         console.log(i + ".单选题:" + right);
 
-            console.log(i+".单选题:"+right);
+         var rightIndex = right.charCodeAt() - 65; // 0开始
+         // 答案里列表
+         var answerListNode =
+            item.children[0].children[0].children[0].children[0].children[1]
+               .children[1].children[0].children[1].children[0].children[0]
+               .children[0].children[0];
+         if (answerListNode.children.length == 1) {
+            // 选中答案
+            try {
+               // 尝试选择判断题/单选题
+               if (rightIndex == 0) {
+                  answerListNode.children[0].children[0].children[0].click();
+               } else if (rightIndex == 1) {
+                  answerListNode.children[0].children[3].children[0].click();
+               } else if (rightIndex == 2) {
+                  answerListNode.children[0].children[6].children[0].click();
+               } else if (rightIndex == 3) {
+                  answerListNode.children[0].children[9].children[0].click();
+               }
+            } catch (e) {}
+         } else {
+            //
+            // 选中答案
+            try {
+               // 尝试选择判断题/单选题
+               answerListNode.rows[rightIndex].children[0].children[0].click();
+            } catch (e) {}
+         }
+      } else {
+         // 多选题
 
-            var rightIndex = right.charCodeAt() - 65;// 0开始
+         console.log(i + ".多选题:" + right);
+
+         for (var j = 0; j < right.length; j++) {
+            var crooet = right[j];
+            var rightIndex = crooet.charCodeAt() - 65;
             // 答案里列表
-            var answerListNode = item.children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[1].children[0].children[0].children[0].children[0]
-            if(answerListNode.children.length == 1){
-                // 选中答案
-                try{// 尝试选择判断题/单选题
-                    if(rightIndex == 0){
-                        answerListNode.children[0].children[0].children[0].click();
-                    } else if(rightIndex == 1){
-                        answerListNode.children[0].children[3].children[0].click();
-                    } else if(rightIndex == 2){
-                        answerListNode.children[0].children[6].children[0].click();
-                    } else if(rightIndex == 3){
-                        answerListNode.children[0].children[9].children[0].click();
-                    }
-                }catch(e){}
-
-            }else{//
-                // 选中答案
-                try{// 尝试选择判断题/单选题
-                    answerListNode.rows[rightIndex].children[0].children[0].click();
-                }catch(e){}
+            var answerListNode =
+               item.children[0].children[0].children[0].children[0].children[1]
+                  .children[1].children[0].children[1].children[0].children[0]
+                  .children[0].children[0];
+            // 选中答案
+            try {
+               // 尝试选择判断题
+               answerListNode.rows[rightIndex].children[0].children[0].click();
+            } catch (e) {
+               console.log(e);
             }
-
-
-        }else{// 多选题
-
-            console.log(i+".多选题:"+right);
-
-            for(var j=0;j<right.length; j++){
-                var crooet = right[j];
-                var rightIndex = crooet.charCodeAt() - 65;
-                // 答案里列表
-                var answerListNode = item.children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[1].children[0].children[0].children[0].children[0]
-
-
-
-
-
-                // 选中答案
-                try{// 尝试选择判断题
-                    answerListNode.rows[rightIndex].children[0].children[0].click();
-                }catch(e){console.log(e)}
-            }
-
-
-        }
-
-
-
-
-    }
-
-
+         }
+      }
+   }
 }
 
+function doFilling(arr) {
+   let elArr = $("#_block_content_exam table tr");
+   let arrOut = [];
+   for (let i = 0; i < elArr.length; i++) {
+      let input = elArr[i].querySelectorAll("input");
+      for (let index = 0; index < input.length; index++) {
+        let itemName=input[index].name;
+        arrOut.push(itemName)
+      }
+   }
+   arrOut.forEach((item,index)=>{
+       $(`#${item}`).val(arr[index])
+   })
+}
